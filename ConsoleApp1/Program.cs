648bed0e8ab798ab8c7dffb217a0b76094196325
Yevgeni <Yevgeni@DESKTOP-BIS8V8E>
2018-10-20 19:01:27 +0300

    003

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,8 +13,17 @@ namespace ConsoleApp1
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Please write \"exit\" to Exit the Program");
+            Console.WriteLine();
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine(DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
 
-            Console.WriteLine(DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
+                exit = Console.ReadLine().ToLower() == "exit";
+            }
+
+            Console.WriteLine("Press any key to Exit");
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
